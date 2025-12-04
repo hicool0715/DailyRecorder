5523220c1c729429f03f1c051f29bf792791925e
hicool0715 <18870405316@163.com>
2025-12-04 20:35:49 +0800

    MainFrame中控件使用错误

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from ttkbootstrap.constants import *
 from BasicFrame import BasicFrame
 class MainFrame(BasicFrame):
     def createFrame(self, root):
-        self.main_frame = ttk.Frame(root, padding=(3, 3, 12, 12))
+        global_frame = ttk.Frame(root, padding=(3, 3, 12, 12))
+        global_frame.grid(column=1, row=2, sticky=(N, W, E, S))
+        ttk.Combobox(global_frame).grid(column=1, row=1, sticky=(N, E))
+        self.main_frame = ttk.Frame(global_frame, padding=(3, 3, 12, 12))
         self.main_frame.grid(column=1, row=2, sticky=(N, W, E, S))
-        ttk.Combobox(self.main_frame).grid(column=1, row=1, sticky=(N, E))
-        interactive_frame = ttk.Frame(self.main_frame, padding=(3, 3, 12, 12))
-        interactive_frame.grid(column=1, row=2, sticky=(N, W, E, S))
         
         from EntranceFrame import EntranceFrame
         self.showFrame(EntranceFrame())
